ff3bbd864c2da6cb521fa745c15ae15a02ff0a45
lidyadwima <lidyadwima@gmail.com>
2026-04-23 20:11:06 +0800

    Menambahkan Gallery

--- pages/galeri.php
+++ pages/galeri.php
@@ -21,9 +21,104 @@
         ← Kembali ke Home
       </a>
     </div>
-
-    Galeri, Lidya Silahkan Mulai Koding! 
     <!-- koding disini -->
+
+
+<section class="py-3">
+
+    <div class="p-5 rounded-4 border bg-white">
+      <div style="width: 60px; height: 3px; background-color: red; margin-bottom: 10px;"></div>
+      <p class="text-uppercase text-danger fw-semibold mb-2" 
+         style="font-size: 14px; letter-spacing: 1px;">
+        Galeri Foto
+      </p>
+
+      <h1 class="fw-bold mb-3" style="font-size: 48px;">
+        Gambar / Foto
+      </h1>
+
+      <p class="text-muted" style="max-width: 800px; line-height: 1.8;">
+        Seperti yang sering dikatakan, gambar dapat mewakili ribuan kata. 
+        Berikut adalah kumpulan foto keindahan Ubud yang menampilkan pesona alam, budaya, 
+        dan kehidupan masyarakatnya.
+      </p>
+    </div>
+</section>
+
+<div class="py-5">
+  <h2 style="font-size: 48px; margin-bottom: 32px;">
+    Pesona Alam <span style="font-weight: 650; text-decoration: underline;">Ubud</span>
+  </h2>
+
+  <div style="display: grid; grid-template-columns: 1fr 1fr; grid-template-rows: 260px 260px; gap: 10px; margin-bottom: 10px;">
+      <div style="grid-row: 1 / 3;">
+          <img src="https://jungleclububud.com/wp-content/uploads/2025/08/water-ubud-palace.jpg" alt="ubud-1" class="rounded-3" style="width:100%; height:100%; object-fit:cover;">
+        </div>
+        <img src="https://nibble-images.b-cdn.net/nibble/original_images/ubud_tegallalang_21d29a18eb.jpg" alt="ubud-2" class="rounded-3" style="width:100%; height:100%; object-fit:cover;">
+        <img src="https://nibble-images.b-cdn.net/nibble/original_images/ubud_monkey_forest_f4aa662a35.jpg" alt="ubud-3" class="rounded-3" style="width:100%; height:100%; object-fit:cover;">
+    </div>
+
+      <div style="display: grid; grid-template-columns: 1fr 1fr; grid-template-rows: 320px 200px; gap: 10px;">
+        <div style="grid-column: 1 / 3;">
+          <img src="https://jungleclububud.com/wp-content/uploads/2025/10/ubud-envato.jpg" alt="ubud-lebar" class="rounded-3" style="width:100%; height:100%; object-fit:cover;">
+        </div>
+        <img src="https://i.pinimg.com/736x/81/0c/57/810c57a0a3dd0b262a71141cfa05616b.jpg" alt="ubud-4" class="rounded-3" style="width:100%; height: 300px; object-fit:cover;">
+        <img src="https://akcdn.detik.net.id/visual/2025/05/14/foto-dok-ubudquadbikingcom-1747221720490_169.jpeg?w=700&q=90" alt="ubud-5" class="rounded-3" style="width:100%; height: 300px; object-fit:cover;">
+      </div>
+</div>
+
+
+
+<div class="py-5" style="margin-top: 90px;">
+  <h2 style="font-size: 48px; margin-bottom: 32px;">
+    Budaya <span style="font-weight: 650; text-decoration: underline;">Ubud</span>
+  </h2>
+
+ 
+  <div style="display: grid; grid-template-columns: 1.5fr 1fr 1fr; gap: 10px; height: 320px; margin-bottom: 10px;">
+    <img src="https://awsimages.detik.net.id/community/media/visual/2026/03/18/antusiasme-para-turis-asing-menunggu-penampilan-ogoh-ogoh-saat-diguyur-hujan-di-catus-pata-ubud-jalan-suweta-ubud-gianyar-rabu-1773833671893_169.jpeg?w=620" 
+         alt="ogoh-ogoh" class="rounded-3" style="width:100%; height:100%; object-fit:cover;">
+    <img src="https://img.merahputih.com/media/52/38/94/523894e0a35dd4b35f857746a115c331.jpg" 
+         alt="budaya-1" class="rounded-3" style="width:100%; height:100%; object-fit:cover;">
+    <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/18/3d/b9/be/the-balinesse-preparing.jpg?w=1400&h=800&s=1" 
+         alt="budaya-2" class="rounded-3" style="width:100%; height:100%; object-fit:cover;">
+  </div>
+
+  <div style="display: grid; grid-template-columns: 1fr 1fr; grid-template-rows: 250px 250px; gap: 10px;">
+    <img src="https://thumbs.dreamstime.com/b/balinese-galungan-festival-ubud-photo-village-mode-92301559.jpg" 
+         alt="galungan" class="rounded-3" style="width:100%; height:100%; object-fit:cover;">
+    <div style="grid-row: 1 / 3;">
+      <img src="https://thumbs.dreamstime.com/b/melasti-ceremony-temple-banjar-bunutan-kedewatan-ubud-bali-indonesia-ubud-bali-melasti-ceremony-banjar-bunutan-242682179.jpg?w=992" 
+           alt="melasti" class="rounded-3" style="width:100%; height:100%; object-fit:cover;">
+    </div>
+    <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/14/72/06/ea/pura-dalem-puri-peliatan.jpg?w=1100&h=1100&s=1" 
+         alt="pura" class="rounded-3" style="width:100%; height:100%; object-fit:cover;">
+  </div>
+</div>
+
+<div class="py-5">
+  <h2 style="font-size: 48px; margin-bottom: 32px;">
+    Kerajinan dan Seni <span style="font-weight: 650; text-decoration: underline;">Ubud</span>
+  </h2>
+
+  <div style="display: grid; grid-template-columns: 1fr 1fr; grid-template-rows: 250px 250px; gap: 10px; margin-bottom: 10px;">
+    <div style="grid-row: 1 / 3;">
+      <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/28/18/19/97/caption.jpg?w=1400&h=800&s=1" 
+           alt="seni-1" class="rounded-3" style="width: 100%; height:100%; object-fit:cover;">
+    </div>
+    <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/b9/60/05/20190927-105909-01-largejpg.jpg?w=1200&h=-1&s=1" 
+         alt="seni-2" class="rounded-3" style="width: 100%; height:100%; object-fit:cover;">
+    <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/b9/60/03/20190927-110511-01-largejpg.jpg?w=1200&h=-1&s=1" 
+         alt="seni-3" class="rounded-3" style="width: 100%; height:100%; object-fit:cover;">
+  </div>
+
+  <div style="display: grid; grid-template-columns: 1fr 1fr; gap: 10px;">
+    <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/25/fa/3b/20190825085047-img-2347.jpg?w=1400&h=800&s=1" 
+         alt="seni-4" class="rounded-3" style="width: 100%; height: 250px; object-fit:cover;">
+    <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/14/6d/f5/1e/ubud-traditional-art.jpg?w=1400&h=800&s=1" 
+         alt="seni-5" class="rounded-3" style="width: 100%; height:250px; object-fit:cover;">
+  </div>
+</div>
 
     <div class="text-center my-4">
       <a href="../index.php" class="text-decoration-none text-muted" style="font-size: 14px; letter-spacing: 1px;">
